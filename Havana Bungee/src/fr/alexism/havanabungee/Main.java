29da4be8fa00001cc0a8a9e33db98a85a85c39fc
AlexisM <al3xdu42@gmail.com>
2021-12-17 22:47:44 +0100

    BungeeCord Plugin

---
+++ Havana Bungee/src/fr/alexism/havanabungee/Main.java
@@ -0,0 +1,24 @@
+package fr.alexism.havanabungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        getProxy().getConsole().sendMessage("Le plugin marche parfaitement !");
+    }
+
+    @Override
+    public void onDisable() {
+        getProxy().getConsole().sendMessage("Le plugin s'arrete correctement");
+    }
+
+    public static Main getInstance(){
+        return instance;
+    }
+}
