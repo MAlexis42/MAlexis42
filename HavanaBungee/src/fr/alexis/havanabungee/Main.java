09f24a90d9f47b9592572adaa4282861feaa20dc
AlexisM <al3xdu42@gmail.com>
2021-12-17 23:39:22 +0100

    BungeeCord Plugin

--- HavanaBungee/src/fr/alexis/havanabungee/Main.java
+++ HavanaBungee/src/fr/alexis/havanabungee/Main.java
@@ -2,6 +2,9 @@ package fr.alexis.havanabungee;
 
 import fr.alexis.havanabungee.listeners.ServerPing;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
@@ -41,10 +44,34 @@ public class Main extends Plugin {
 
                 file.createNewFile();
 
+                if(filename.equals("mysql")){
+                    Configuration config = getConfig(filename);
+                    config.set("mysql.host", "localhost");
+                    saveConfig(config, filename);
+                }
+
             }catch(IOException e){
 
                 e.printStackTrace();
             }
+        }
+    }
+
+    public Configuration getConfig(String fileName){
+        try{
+
+        return ConfigurationProvider.getProvider(YamlConfiguration.class).load((new File(getDataFolder(), fileName + ".yml")));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void saveConfig(Configuration config, String fileName){
+        try {
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, new File(getDataFolder(), fileName + ".yml"));
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
